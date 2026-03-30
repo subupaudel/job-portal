d47d8901cf8a01640cbefaf92a07fc96ccab3739
subodhanaa <subodhana.paudel@wowfinstacknepal.com>
2026-03-30 12:51:13 +0545

    feat: error handling

--- src/main/java/com/example/jobportal/security/CorsConfig.java
+++ src/main/java/com/example/jobportal/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOriginPatterns("*")
+                        .allowedOrigins("https://27.34.72.200:3000")
                         .allowedHeaders("*")
                 .allowCredentials(true);
             }
